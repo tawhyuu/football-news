66f85b4a870b03aa72b0493a8f1e3d2e438e23bf
Wahyuga <tawahyuga17@gmail.com>
2025-09-10 14:22:19 +0700

    Add news management features with forms and templates
    - Implemented NewsForm for creating news articles.
    - Added create_news view to handle news creation.
    - Created templates for adding news and displaying news details.
    - Updated main template to include news list and navigation.
    - Configured URL patterns for news-related views.
    - Enhanced settings for CSRF trusted origins.

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,7 +31,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 # tangguh.ambha = tangguh-ambha-footballnews
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "tangguh-ambha-footballnews.pbp.cs.ui.ac.id"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "http://tangguh-ambha-footballnews.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
